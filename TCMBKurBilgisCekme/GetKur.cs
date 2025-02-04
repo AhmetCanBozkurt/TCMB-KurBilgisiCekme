89675ac31e4d1a0bfdc7bd7f06b7ea6fbcb106bd
Ahmet Can Bozkurt <ahmetcanb785@gmail.com>
2025-02-05 01:56:36 +0300

    first commit

---
+++ TCMBKurBilgisCekme/GetKur.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TCMBKurBilgisCekme
+{
+    public class GetKur
+    {
+        public decimal[] GetKurList()
+        {
+
+            decimal[] array = new decimal[4];
+            DataSet dataSet = new DataSet();
+            dataSet.ReadXml("https://www.tcmb.gov.tr/kurlar/today.xml");
+
+            //Dolar için alanlar
+            DataRow dataRow = dataSet.Tables[1].Rows[0];
+
+            array[0] = Convert.ToDecimal(dataRow[3].ToString().Replace('.',',')); // Alış fiyatı 
+            array[1] = Convert.ToDecimal(dataRow[4].ToString().Replace('.',',')); // Satış Fiyatı
+
+
+
+            //Euro için
+            dataRow = dataSet.Tables[1].Rows[3];
+            array[2] = Convert.ToDecimal(dataRow[3].ToString().Replace('.',',')); // Alış fiyatı 
+            array[3] = Convert.ToDecimal(dataRow[4].ToString().Replace('.',','));// Satış Fiyatı
+
+
+
+            return array;
+
+        }
+    }
+}
---
+++ TestProjesi/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using TCMBKurBilgisCekme;
+
+namespace TestProjesi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            GetKur kur = new GetKur();
+
+
+            foreach (var item in kur.GetKurList()) {
+
+                Console.WriteLine(item);
+            
+            }
+        
+            Console.ReadLine();
+
+
+
+                 }
+    }
+}
